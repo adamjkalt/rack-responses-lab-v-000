457a66903ad20bff11a6e8d06774601c3336e993
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-08 03:30:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,10 +6,6 @@ class Application
     num_1 = Kernel.rand(1..12)
     num_2 = Kernel.rand(13..24)
 
-    resp.write "#{num_1}\n"
-    resp.write "#{num_2}\n"
-    resp.write "#{num_3}\n"
-
     if num_1
       resp.write "Good Morning!"
     else
